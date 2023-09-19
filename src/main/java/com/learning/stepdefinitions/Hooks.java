c7ce83fd67aef6d02422800e0b51b11ae6db6c10
Nagarjuna SK <65765913+NagarjunaSK-Git@users.noreply.github.com>
2023-09-19 13:04:48 +0530

    Fixing build failures.
    Updated Dependencies to the latest versions and fixed browser driver's compatibility (#5)

--- src/main/java/com/learning/stepdefinitions/Hooks.java
+++ src/main/java/com/learning/stepdefinitions/Hooks.java
@@ -4,8 +4,6 @@ import com.learning.base.SeleniumBase;
 import com.learning.context.TestContextShared;
 import com.learning.factory.BrowserDriverFactory;
 import io.cucumber.java.*;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 public class Hooks {
